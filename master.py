adad64dcb6ad89d96ea3578b243274534a794edc
Taweechai Yuenyang <krumii.it@gmail.com>
2022-05-13 17:01:50 +0700

    update master

--- master.py
+++ master.py
@@ -20,47 +20,47 @@ ORA_DNS=f"{os.environ.get('ORAC_DB_HOST')}/{os.environ.get('ORAC_DB_SERVICE')}"
 ORA_USERNAME=os.environ.get('ORAC_DB_USERNAME')
 ORA_PASSWORD=os.environ.get('ORAC_DB_PASSWORD')
 
-# def update_master_location():
-#     mydb = pgsql.connect(
-#         host=DB_HOSTNAME,
-#         port=DB_PORT,
-#         user=DB_USERNAME,
-#         password=DB_PASSWORD,
-#         database=DB_NAME,
-#     )
+def update_die():
+    mydb = pgsql.connect(
+        host=DB_HOSTNAME,
+        port=DB_PORT,
+        user=DB_USERNAME,
+        password=DB_PASSWORD,
+        database=DB_NAME,
+    )
     
-#     Oracon = cx_Oracle.connect(user=ORA_PASSWORD,password=ORA_USERNAME,dsn=ORA_DNS)
-#     Oracur = Oracon.cursor()
+    Oracon = cx_Oracle.connect(user=ORA_PASSWORD,password=ORA_USERNAME,dsn=ORA_DNS)
+    Oracur = Oracon.cursor()
         
-#     try:
-#         log(name='MASTER', subject="Start", status="Success", message=f"Sync Shelve")    
-#         mycursor = mydb.cursor()
-#         mycursor.execute("""select t.id,t.die_no,t.serial_no  from tbt_cartons t where t.die_no='None' order by t.serial_no""")
+    try:
+        log(name='MASTER', subject="Start", status="Success", message=f"Sync Shelve")    
+        mycursor = mydb.cursor()
+        mycursor.execute("""select t.id,t.die_no,t.serial_no  from tbt_cartons t where t.die_no='None' order by t.serial_no""")
         
-#         data = mycursor.fetchall()
-#         for i in data:
-#             carton_id = str(i[0])
-#             die_no = str(i[1])
-#             serial_no = str(i[2])
-#             ora_sql = f"""SELECT CASEID,CASENO  FROM TXP_CARTONDETAILS WHERE RUNNINGNO='{serial_no}'"""
-#             obj = Oracur.execute(ora_sql)
-#             for x in obj.fetchall():
-#                 die_no = str(x[0])
-#                 division_no = str(x[1]).replace('None', '')
-#                 #### create carton on stock Cloud
-#                 mycursor.execute(f"update tbt_cartons set die_no='{die_no}', division_no='{division_no}' where id='{carton_id}'")
+        data = mycursor.fetchall()
+        for i in data:
+            carton_id = str(i[0])
+            die_no = str(i[1])
+            serial_no = str(i[2])
+            ora_sql = f"""SELECT CASEID,CASENO  FROM TXP_CARTONDETAILS WHERE RUNNINGNO='{serial_no}'"""
+            obj = Oracur.execute(ora_sql)
+            for x in obj.fetchall():
+                die_no = str(x[0])
+                division_no = str(x[1]).replace('None', '')
+                #### create carton on stock Cloud
+                mycursor.execute(f"update tbt_cartons set die_no='{die_no}', division_no='{division_no}' where id='{carton_id}'")
                 
-#         mydb.commit()
-#         log(name='MASTER', subject="END", status="Success", message=f"Sync Shelve")    
-#     except Exception as ex:
-#         log(name='MASTER', subject="UPLOAD SHELVE", status="Error", message=str(ex))
-#         Oracon.rollback()
-#         mydb.rollback()
-#         pass
+        mydb.commit()
+        log(name='MASTER', subject="END", status="Success", message=f"Sync Shelve")    
+    except Exception as ex:
+        log(name='MASTER', subject="UPLOAD SHELVE", status="Error", message=str(ex))
+        Oracon.rollback()
+        mydb.rollback()
+        pass
     
-#     # Oracon.commit()
-#     Oracon.close()
-#     mydb.close()
+    # Oracon.commit()
+    Oracon.close()
+    mydb.close()
 
 def update_master_location():
     mydb = pgsql.connect(
@@ -97,4 +97,6 @@ def update_master_location():
     
 if __name__ == '__main__':
     update_master_location()
+    time.sleep(0.1)
+    update_die()
     sys.exit(0)
