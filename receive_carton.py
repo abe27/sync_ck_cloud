39d8e5b002c01e6a437356ca2a0176080d3c7a75
Taweechai Yuenyang <krumii.it@gmail.com>
2022-05-13 16:31:55 +0700

    remove limit

--- receive_carton.py
+++ receive_carton.py
@@ -43,8 +43,7 @@ def main():
             select cc.receive_detail_id,count(cc.receive_detail_id) ctn from tbt_cartons cc group by cc.receive_detail_id
         ) c on d.id = c.receive_detail_id 
         where t.receive_date >= (current_date - 1) and d.plan_ctn > (case when c.ctn is null then 0 else c.ctn end) and t.receive_no like 'TI%'
-        order by t.receive_date,t.receive_no,p.no,p.name
-        limit 20""")
+        order by t.receive_date,t.receive_no,p.no,p.name""")
         
         rnd_x = 1
         data = mycursor.fetchall()
