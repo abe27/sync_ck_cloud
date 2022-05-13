3c6c59122f2eb50dc9a135ef732f909ae19f7b57
Taweechai Yuenyang <krumii.it@gmail.com>
2022-05-13 12:00:07 +0700

    update makelog folder

--- spllibs.py
+++ spllibs.py
@@ -12,7 +12,9 @@ from shareplum.site import Version
 class LogActivity:
     def __init__(self, name='CK', subject=None, status='Active', message=None):
         d = datetime.now().strftime('%Y%m%d')
-        filename = f"{name}-{d}.log"
+        path = "LOGS"
+        if os.path.exists(path) is False:os.mkdir(path)
+        filename = f"{path}/{name}-{d}.log"
         f = open(filename, 'a+')
         txt = f"{str(datetime.now().strftime('%Y%m%d %H:%M:%S')).ljust(25)}SUBJECT: {str(subject).ljust(20)}STATUS: {str(status).ljust(10)}MESSAGE: {str(message).ljust(50)}"
         f.write(f"{txt}\n")
