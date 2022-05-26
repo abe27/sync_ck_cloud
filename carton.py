c3787da7038e95e4333d40b48ea3270d82008f72
Parzival Yuenyang <krumii.it@gmail.com>
2022-05-26 11:34:11 +0700

    update sync carton

--- carton.py
+++ carton.py
@@ -58,31 +58,54 @@ def main():
     part_stk = None
     i = 1
     for r in obj.fetchall():
-        whs = str(r[0])
-        factory = str(r[1])
-        rec_date = str(r[2])
+        whs = str(r[0]).strip()
+        factory = str(r[1]).strip()
+        rec_date = str(r[2]).strip()
         invoice_no = str(r[3]).strip()
-        rvmanagingno = str(r[4])
-        part_type = str(r[5])
-        part_no = str(r[6])
+        rvmanagingno = str(r[4]).strip()
+        part_type = str(r[5]).strip()
+        part_no = str(r[6]).strip()
         part_name = str(r[7]).replace("'", "''")
-        unit = str(r[8])
-        serial_no = str(r[9])
-        lot_no = str(r[10])
-        case_id = str(r[11])
+        unit = str(r[8]).strip()
+        serial_no = str(r[9]).strip()
+        lot_no = str(r[10]).strip()
+        case_id = str(r[11]).strip()
         case_no = int(str(r[12]))
         std_pack_qty = int(str(r[13]))
         qty = int(str(r[14]))
-        shelve = str(r[15])
-        pallet_no = str(r[16])
+        shelve = str(r[15]).strip()
+        pallet_no = str(r[16]).strip()
         on_stock = int(str(r[17]))
         on_stock_ctn = int(str(r[18]))
         event_trigger = str(r[19])
-        older_key = str(r[20])
-        siid = str(r[21])
+        older_key = str(r[20]).strip()
+        siid = str(r[21]).strip()
         
         ### Get Master Data
+        pg_cursor.execute(f"select id from tbt_tagrps where name='C'")
+        tagrp_id = pg_cursor.fetchone()[0]
         
+        pg_cursor.execute(f"select id from tbt_part_types where name='{part_type}'")
+        part_type_id = pg_cursor.fetchone()[0]
+        
+        pg_cursor.execute(f"select id from tbt_whs where name='{whs}'")
+        whs_id = pg_cursor.fetchone()[0]
+        
+        pg_cursor.execute(f"select id from tbt_factory_types where name='{factory}'")
+        fac_id = pg_cursor.fetchone()[0]
+        
+        pg_cursor.execute(f"select id from tbt_units where name='{unit}'")
+        unit_id = pg_cursor.fetchone()[0]
+        
+        pg_cursor.execute(f"select id from tbt_locations where name='{shelve}'")
+        shelve_id = generate(size=36)
+        sh = pg_cursor.fetchone()
+        sql_shelve = f"insert into tbt_locations(id,name,description,is_active,created_at,updated_at)values('{shelve_id}','{shelve}','{shelve}',true,current_timestamp,current_timestamp)"
+        if sh:
+            shelve_id = sh[0]
+            sql_shelve = f"update tbt_locations set updated_at=current_timestamp where id='{shelve_id}'"
+            
+        pg_cursor.execute(sql_shelve)
 
         ### check stock
         if part_stk is None: part_stk = part_no
@@ -99,11 +122,44 @@ def main():
             
         pg_cursor.execute(sql_part)
         
+        ### check ledger
+        pg_cursor.execute(f"select id from tbt_ledgers where part_type_id='{part_type_id}' and tagrp_id='{tagrp_id}' and factory_id='{fac_id}' and whs_id='{whs_id}' and part_id='{part_id}'")
+        ledger = pg_cursor.fetchone()
+        ledger_id = generate(size=36)
+        sql_ledger = f"""insert into tbt_ledgers(id, part_type_id, tagrp_id, factory_id, whs_id, part_id, kinds_id, sizes_id, colors_id, width, length, height, net_weight, gross_weight, unit_id, is_active, created_at, updated_at)values('{ledger_id}', '{part_type_id}', '{tagrp_id}', '{fac_id}', '{whs_id}', '{part_id}', null, null, null, 0, 0, 0, 0, 0, '{unit_id}', true, current_timestamp, current_timestamp)"""
+        if ledger:
+            ledger_id = ledger[0]
+            sql_ledger = f"update tbt_ledgers set updated_at=current_timestamp where id='{ledger_id}'"
+            
+        pg_cursor.execute(sql_ledger)
         
+        ### check carton
+        pg_cursor.execute(f"select id from tbt_cartons where ledger_id='{ledger_id}'")
+        carton = pg_cursor.fetchone()
+        carton_id = generate(size=36)
+        sql_carton = f"""insert into tbt_cartons(id,ledger_id,lot_no,serial_no,die_no,revision_no,qty,is_active,created_at,updated_at)values('{carton_id}','{ledger_id}','{lot_no}','{serial_no}','{case_id}','{case_no}','{qty}',true,current_timestamp,current_timestamp)"""
+        if carton:
+            carton_id = carton[0]
+            sql_carton = f"""update tbt_cartons set ledger_id='{ledger_id}',lot_no='{lot_no}',serial_no='{serial_no}',die_no='{case_id}',revision_no='{case_no}',qty='{qty}',is_active=true,updated_at=current_timestamp where id='{carton_id}'"""
+        pg_cursor.execute(sql_carton)
         
-        print(f"{i} ==> part: {part_no} serial no: {serial_no}")
+        ### check stock
+        ctn = 1
+        if qty == 0:ctn = 1
+        pg_cursor.execute(f"select id from tbt_stocks where ledger_id='{ledger_id}'")
+        stock = pg_cursor.fetchone()
+        stock_id = generate(size=36)
+        sql_stock = f"""insert into tbt_stocks(id, ledger_id, per_qty, ctn, is_active, created_at, updated_at)values('{stock_id}', '{ledger_id}', {std_pack_qty}, {ctn}, true, current_timestamp, current_timestamp)"""
+        if stock:
+            stock_id = stock[0]
+            sql_stock = f"""update tbt_stocks set ctn=ctn+{ctn},is_active=true,updated_at=current_timestamp where id='{stock_id}'"""
+        pg_cursor.execute(sql_stock)
+        
+        Ora.execute(f"UPDATE TXP_CARTONDETAILS SET IS_CHECK=1 WHERE RUNNINGNO='{serial_no}'")
+        print(f"{i} ==> part: {part_no} serial no: {serial_no} qty: {qty} ctn: {ctn}")
         i += 1
-
+        
+    pgdb.commit()
     pgdb.close()
 
 if __name__ == '__main__':
@@ -111,6 +167,7 @@ if __name__ == '__main__':
         main()
     except Exception as ex:
         print(ex)
+        Conn.rollback()
         pass
     
     Conn.commit()
