60f2ac318db4d6fb560366f4a7350c6e27f7aafd
Parzival Yuenyang <krumii.it@gmail.com>
2022-07-11 13:21:28 +0700

    update sync prepare

--- sync_invoice.py
+++ sync_invoice.py
@@ -209,8 +209,155 @@ def main():
         Oracon.commit()
         pgdb.commit()
         
+def sync_carton(serial_no):
+    sql = f"""SELECT 'CK-2' whs,CASE WHEN substr(t.PARTNO, 1, 1) = '1' THEN 'AW' ELSE 'INJ' END factory,to_char(t.SYSDTE, 'YYYY-MM-DD')  rec_date,t.INVOICENO  invoice_no,t.RVMANAGINGNO,CASE WHEN p.TYPE IS NULL THEN '-' ELSE CASE WHEN p.TYPE = 'PRESS' THEN 'PLATE' ELSE p.TYPE END END  part_type,t.PARTNO part_no,p.PARTNAME,'BOX' unit,t.RUNNINGNO serial_no,t.LOTNO lot_no,t.CASEID case_id,CASE WHEN t.CASENO IS NULL THEN 0 ELSE t.CASENO END case_no,t.RECEIVINGQUANTITY std_pack_qty,t.RECEIVINGQUANTITY qty,t.SHELVE shelve,CASE WHEN t.PALLETKEY IS NULL THEN '-' ELSE t.PALLETKEY END pallet_no,0 on_stock, 0 on_stock_ctn,'R' event_trigger,'-' olderkey,CASE WHEN t.SIID IS NULL THEN 'NO' ELSE t.SIID END SIID
+            FROM TXP_CARTONDETAILS t
+            INNER JOIN TXP_PART p ON t.PARTNO=p.PARTNO  
+            WHERE t.RUNNINGNO='{serial_no}'"""
+    # print(sql)
+    obj = Oracur.execute(sql)
+    
+    i = 1
+    for r in obj.fetchall():
+        whs = str(r[0]).strip()
+        factory = str(r[1]).strip()
+        rec_date = str(r[2]).strip()
+        invoice_no = str(r[3]).strip()
+        rvmanagingno = str(r[4]).strip()
+        part_type = str(r[5]).strip()
+        part_no = str(r[6]).strip()
+        part_name = str(r[7]).replace("'", "''")
+        unit = str(r[8]).strip()
+        serial_no = str(r[9]).strip()
+        lot_no = str(r[10]).strip()
+        case_id = str(r[11]).strip()
+        case_no = int(str(r[12]))
+        std_pack_qty = int(str(r[13]))
+        qty = int(str(r[14]))
+        shelve = str(r[15]).strip()
+        pallet_no = str(r[16]).strip()
+        on_stock = int(str(r[17]))
+        on_stock_ctn = int(str(r[18]))
+        event_trigger = str(r[19])
+        older_key = str(r[20]).strip()
+        siid = str(r[21]).strip()
+        
+        ### Get Master Data
+        pg_cursor.execute(f"select id from tbt_tagrps where name='C'")
+        tagrp_id = pg_cursor.fetchone()[0]
+        
+        pg_cursor.execute(f"select id from tbt_part_types where name='{part_type}'")
+        part_type_id = pg_cursor.fetchone()[0]
+        
+        pg_cursor.execute(f"select id from tbt_whs where name='{whs}'")
+        whs_id = pg_cursor.fetchone()[0]
+        
+        pg_cursor.execute(f"select id from tbt_factory_types where name='{factory}'")
+        fac_id = pg_cursor.fetchone()[0]
+        
+        pg_cursor.execute(f"select id from tbt_units where name='{unit}'")
+        unit_id = pg_cursor.fetchone()[0]
+        
+        pg_cursor.execute(f"select id from tbt_locations where name='{shelve}'")
+        shelve_id = generate(size=36)
+        sh = pg_cursor.fetchone()
+        sql_shelve = f"insert into tbt_locations(id,name,description,is_active,created_at,updated_at)values('{shelve_id}','{shelve}','{shelve}',true,current_timestamp,current_timestamp)"
+        if sh:
+            shelve_id = sh[0]
+            sql_shelve = f"update tbt_locations set updated_at=current_timestamp where id='{shelve_id}'"
+            
+        pg_cursor.execute(sql_shelve)
+        
+        ### check part
+        pg_cursor.execute(f"select id from tbt_parts where no='{part_no}'")
+        part = pg_cursor.fetchone()
+        part_id = generate(size=36)
+        sql_part = f"insert into tbt_parts(id, no,name, is_active,created_at,updated_at)values('{part_id}','{part_no}','{part_name}',true,current_timestamp,current_timestamp)"
+        if part:
+            part_id = part[0]
+            sql_part = "update tbt_parts set updated_at=current_timestamp where id = '{part_id}'"
+            
+        pg_cursor.execute(sql_part)
+        
+        ### check ledger
+        pg_cursor.execute(f"select id from tbt_ledgers where part_type_id='{part_type_id}' and tagrp_id='{tagrp_id}' and factory_id='{fac_id}' and whs_id='{whs_id}' and part_id='{part_id}'")
+        ledger = pg_cursor.fetchone()
+        ledger_id = generate(size=36)
+        sql_ledger = f"""insert into tbt_ledgers(id, part_type_id, tagrp_id, factory_id, whs_id, part_id, kinds_id, sizes_id, colors_id, width, length, height, net_weight, gross_weight, unit_id, is_active, created_at, updated_at)values('{ledger_id}', '{part_type_id}', '{tagrp_id}', '{fac_id}', '{whs_id}', '{part_id}', null, null, null, 0, 0, 0, 0, 0, '{unit_id}', true, current_timestamp, current_timestamp)"""
+        if ledger:
+            ledger_id = ledger[0]
+            sql_ledger = f"update tbt_ledgers set updated_at=current_timestamp where id='{ledger_id}'"
+            
+        pg_cursor.execute(sql_ledger)
+        
+        ### check carton
+        pg_cursor.execute(f"select id from tbt_cartons where serial_no='{serial_no}'")
+        carton = pg_cursor.fetchone()
+        carton_id = generate(size=36)
+        sql_carton = f"""insert into tbt_cartons(id,ledger_id,lot_no,serial_no,die_no,revision_no,qty,is_active,created_at,updated_at)values('{carton_id}','{ledger_id}','{lot_no}','{serial_no}','{case_id}','{case_no}','{qty}',true,current_timestamp,current_timestamp)"""
+        if carton:
+            carton_id = carton[0]
+            sql_carton = f"""update tbt_cartons set ledger_id='{ledger_id}',lot_no='{lot_no}',serial_no='{serial_no}',die_no='{case_id}',revision_no='{case_no}',qty='{qty}',is_active=true,updated_at=current_timestamp where id='{carton_id}'"""
+        pg_cursor.execute(sql_carton)
+        
+        ### check carton on shelve
+        pg_cursor.execute(f"select id from tbt_shelves where carton_id='{carton_id}' and location_id='{shelve_id}'")
+        carton_shelve = pg_cursor.fetchone()
+        carton_shelve_id = generate(size=36)
+        sql_shelve = f"insert into tbt_shelves(id, carton_id, location_id, pallet_no, is_printed, is_active, created_at, updated_at)values('{carton_shelve_id}', '{carton_id}', '{shelve_id}', '{pallet_no}', false, true, current_timestamp, current_timestamp)"
+        if carton_shelve:
+            carton_shelve_id = carton_shelve[0]
+            sql_shelve = f"update tbt_shelves set location_id='{shelve_id}',pallet_no='{pallet_no}',is_printed=true,is_active=true,updated_at=current_timestamp where id='{carton_shelve_id}'"
+        pg_cursor.execute(sql_shelve)
+        
+        ### check stock
+        stk_carton = Oracur.execute(f"SELECT count(PARTNO) FROM TXP_CARTONDETAILS WHERE PARTNO='{part_no}' AND SHELVE NOT IN ('S-PLOUT', 'S-XXX', 'S-CK1')")
+        ctn = stk_carton.fetchone()[0]
+        
+        ### stock form pg
+        pg_cursor.execute(f"select id from tbt_stocks where ledger_id='{ledger_id}'")
+        stock = pg_cursor.fetchone()
+        stock_id = generate(size=36)
+        sql_stock = f"""insert into tbt_stocks(id, ledger_id, per_qty, ctn, is_active, created_at, updated_at)values('{stock_id}', '{ledger_id}', {std_pack_qty}, {ctn}, true, current_timestamp, current_timestamp)"""
+        if stock:
+            stock_id = stock[0]
+            sql_stock = f"""update tbt_stocks set ctn={ctn},is_active=true,updated_at=current_timestamp where id='{stock_id}'"""
+        pg_cursor.execute(sql_stock)
+        
+        Oracur.execute(f"UPDATE TXP_CARTONDETAILS SET IS_CHECK=1 WHERE RUNNINGNO='{serial_no}'")
+        print(f"{i} ==> part: {part_no} serial no: {serial_no} qty: {qty} ctn: {ctn}")
+        i += 1
+
+    return True
+        
+def sync_pallet_scan():
+    sql = f"""SELECT ts.RUNNINGNO,tc.RUNNINGNO,ti.PLOUTNO FROM TMP_SCANOUT ts LEFT JOIN TXP_ISSPACKDETAIL ti ON ts.RUNNINGNO=ti.FTICKETNO LEFT JOIN TXP_CARTONDETAILS tc ON ti.CTNSN=tc.RUNNINGNO """
+    Oracur.execute(sql)
+    for i in Oracur.fetchall():
+        fticket_no = str(i[0]).strip()
+        serial_no = str(i[1]).strip()
+        plout_no = str(i[2]).strip()
+        pg_cursor.execute(f"select id from tbt_cartons where serial_no='{serial_no}'")
+        carton_db = pg_cursor.fetchone()
+        carton_id = None
+        if carton_db is None:
+            sync_carton(serial_no)
+            pg_cursor.execute(f"select id from tbt_cartons where serial_no='{serial_no}'")
+            carton_id = pg_cursor.fetchone()[0]
+        else:    
+            carton_id = carton_db[0]
+            
+        pg_cursor.execute(f"update tbt_ftickets set carton_id='{carton_id}',pl_out_no='{plout_no}' where fticket_no='{fticket_no}'")
+        Oracur.execute(f"UPDATE TMP_SCANOUT SET SYNC=1 WHERE RUNNINGNO='{fticket_no}' AND SYNC=0")
+        Oracon.commit()
+        pgdb.commit()
+        
+    Oracur.execute(f"DELETE FROM TMP_SCANOUT WHERE SYNC=1")
+    Oracon.commit()
+        
 if __name__ == '__main__':
     main()
+    sync_pallet_scan()
     pgdb.close()
     pool.release(Oracon)
     pool.close()
