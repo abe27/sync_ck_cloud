0a045a2c9c747bc46588e2bdf006164e5e0fcb7c
Parzival Yuenyang <krumii.it@gmail.com>
2022-07-11 14:24:04 +0700

    update get gedi

--- gedi.py
+++ gedi.py
@@ -302,8 +302,8 @@ def main():
             shutil.move(txt_filename, dist)
             pg_cursor.execute(f"update tbt_invoices set invoice_status='H',is_send_gedi=true where id='{r['primary_id']}'")
             pg_db.commit()
-            msg = f"test export GEDI {txt_filename} is completed!"
-            # spl.line_notification(msg)
+            msg = f"Test export GEDI {txt_filename} is completed!"
+            spl.line_notification(msg)
             print(msg)
             i += 1
             
